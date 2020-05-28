1e30072d32622a42ffd16a7873fb800d1dab4c7b
2ndyrGroupC <2ndyrGroupC@pn.local>
2020-05-28 11:25:29 +0800

    adding homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { HomepageComponent } from "./homepage/homepage.component";
 
 const routes: Routes = [
+  {path:'', component:HomepageComponent},
   {path:'admin', component:LoginComponent},
   {path:'**', redirectTo:''}
 ];
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import 'jarallax';
+declare var jarallax: any;
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit,  AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.2
+    });
   }
 
 }
